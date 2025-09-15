b3be0e07b2157172d4de80ed8743a01cdf81d64b
parvaneh-sharghi <parvaneh.sharghi@gmail.com>
2025-09-15 19:20:02 -0400

    Adding Get Endpoints

--- VulnLite API/app/main.py
+++ VulnLite API/app/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlmodel import select
+from fastapi import HTTPException
+
 
 from .db import init_db, get_session, Vulnerability
 
@@ -26,3 +29,23 @@ async def create_vuln(
     await session.commit()      # Commit the transaction
     await session.refresh(vuln) # Refresh to get ID and DB-generated values
     return vuln                 # Return the object as JSON response
+
+
+@app.get("/vulns")
+async def list_vulns(session: AsyncSession = Depends(get_session)):
+    # run a SELECT query to fetch all rows
+    result = await session.execute(select(Vulnerability))
+    items = result.scalars().all()  # convert result to list of ORM objects
+    return items                    # FastAPI will return JSON
+
+# Get a single vulnerability by ID
+@app.get("/vulns/{vuln_id}")
+async def get_vuln(vuln_id: int, session: AsyncSession = Depends(get_session)):
+    result = await session.execute(
+        select(Vulnerability).where(Vulnerability.id == vuln_id)
+    )
+    vuln = result.scalars().first()
+    if not vuln:
+        # return 404 if not found
+        raise HTTPException(status_code=404, detail="Vulnerability not found")
+    return vuln
