e7f3555b0dd6750b40d0d490011c2b9ce1b610f1
parvaneh-sharghi <parvaneh.sharghi@gmail.com>
2025-09-26 18:32:23 -0400

    Adding Web Container/ uvicorn web server

--- VulnLite API/app/config.py
+++ VulnLite API/app/config.py
@@ -3,5 +3,5 @@ import os
 # app/config.py
 DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql+asyncpg://postgres:postgres@localhost:5432/vuln"
+    "postgresql+asyncpg://postgres:postgres@db:5432/vuln"
 )
